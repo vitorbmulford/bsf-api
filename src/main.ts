0e51a0f98b5608bf060115cac8f86980874ba776
Cezar Bacanieski <bacanieskidev@gmail.com>
2025-05-22 15:12:48 -0300

    feat: servindo pasta uplaods estaticamente para o front

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   dotenv.config();
   console.log('Starting application...');
   console.log('Environment:', process.env.NODE_ENV);
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   app.enableCors({
     origin: 'http://localhost:3000',
