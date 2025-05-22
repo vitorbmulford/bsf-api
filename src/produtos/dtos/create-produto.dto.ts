2a33e99fb6ddf03cdad7f2a22cdf3df07f2f99eb
vitorbmulford <vitorbmulford@gmail.com>
2025-05-22 14:24:10 -0300

    tirando example

--- src/produtos/dtos/create-produto.dto.ts
+++ src/produtos/dtos/create-produto.dto.ts
@@ -13,51 +13,45 @@ import { Type } from 'class-transformer';
 import { StatusProduto } from '../entities/produto.entity';
 
 export class CriarProdutoDto {
-  @ApiProperty({ example: 'Hambúrguer de Fraldinha' })
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   nome!: string;
 
-  @ApiProperty({ example: 24.9 })
+  @ApiProperty()
   @Type(() => Number)
   @IsNumber()
   @Min(0)
   preco!: number;
 
-  @ApiProperty({ example: 20.9, required: false })
+  @ApiProperty({ required: false })
   @Type(() => Number)
   @IsNumber()
   @Min(0)
   @IsOptional()
   precoPromocional?: number;
 
-  @ApiProperty({
-    example: 'Delicioso hambúrguer de fraldinha com queijo cheddar',
-  })
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   descricao!: string;
 
-  @ApiProperty({ example: 'https://link-da-imagem.com/imagem.png' })
+  @ApiProperty()
   @IsUrl()
   imagemUrl!: string;
 
-  @ApiProperty({ example: 10, required: false })
+  @ApiProperty({ required: false })
   @IsNumber()
   @Min(0)
   @IsOptional()
   estoque?: number;
 
-  @ApiProperty({ example: 'Lanches', required: false })
+  @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
   categoria?: string;
 
-  @ApiProperty({
-    example: StatusProduto.ATIVO,
-    enum: StatusProduto,
-    required: false,
-  })
+  @ApiProperty({ enum: StatusProduto, required: false })
   @IsEnum(StatusProduto)
   @IsOptional()
   status?: StatusProduto;
